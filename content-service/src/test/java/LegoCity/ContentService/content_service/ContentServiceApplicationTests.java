a7cd08f402020b6a569e2eb16dfb45c213431f0e
unknown <hoang1303x@gmail.com>
2026-05-18 22:00:57 +0200

    Project init

---
+++ content-service/src/test/java/LegoCity/ContentService/content_service/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package LegoCity.ContentService.content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
